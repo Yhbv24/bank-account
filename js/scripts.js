8fe720fb3e62e971108951ce01b801035ad40b63
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-01-31 18:35:05 -0800

    Add fully working form, hide form when user clicks submit, center deposit/withdraw form

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Account(name, balance) {
 
 Account.prototype.initDeposit = function(balance) {
   return this.balance += balance;
-}
+};
 
 Account.prototype.withdraw = function(withdrawAmount) {
   return this.balance -= withdrawAmount;
@@ -23,6 +23,10 @@ $(function() {
   $("#bank-form").submit(function(event) {
     event.preventDefault();
 
+    $("#bank-form").hide();
+    $(".col-md-6").addClass("col-md-12");
+    $(".col-md-12").removeClass("col-md-6");
+
     var name = $("#name").val();
     var balance = parseFloat($("#init-deposit").val());
     var newAccount = new Account(name, balance);
@@ -31,16 +35,32 @@ $(function() {
 
     $("#deposit-send").click(function() {
       var depositAmount = parseFloat($("#deposit").val());
+
+      if (!depositAmount) {
+        alert("Please enter an amount you would like to deposit.");
+        codeBreak;
+      }
+
       newAccount.deposit(depositAmount);
 
-      $("#log").append("<li>Name: " + newAccount.name + " | Balance: $" + newAccount.balance.toFixed(2) + "</li>");
+      $("#log").append("<li>Name: " + newAccount.name + " | Balance: $" + newAccount.balance.toFixed(2) + " | Deposited: $" + depositAmount.toFixed(2) + "</li>");
+
+      $("#deposit").val("")
     });
 
     $("#withdraw-send").click(function() {
       var withdrawAmount = parseFloat($("#withdraw").val());
+
+      if (!withdrawAmount) {
+        alert("Please enter an amount you would like to withdraw.");
+        codeBreak;
+      }
+
       newAccount.withdraw(withdrawAmount);
 
-      $("#log").append("<li>Name: " + newAccount.name + " | Balance: $" + newAccount.balance.toFixed(2) + "</li>");
+      $("#log").append("<li>Name: " + newAccount.name + " | Balance: $" + newAccount.balance.toFixed(2) + " | Withdrew: $" + withdrawAmount.toFixed(2) + "</li>");
+
+      $("#withdraw").val("")
     });
   });
 
